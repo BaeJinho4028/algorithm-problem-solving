4ac03321643dadab8e730966f7d88e28d424c6a7
배진호 <onionring4028@gmail.com>
2025-02-21 02:09:01 +0900

    [level 2] Title: 전화번호 목록, Time: 78.64 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,15 +1,16 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 bool solution(vector<string> phone_book) {
-    bool answer = true;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     
     sort(phone_book.begin(), phone_book.end());
     
-    for(int i=1; i<phone_book.size(); i++){
-        if(phone_book[i-1] == phone_book[i].substr(0, phone_book[i-1].size())) return false;
+    for(int i = 1; i < phone_book.size(); ++i) {
+        if(phone_book[i-1] == phone_book[i].substr(0, phone_book[i-1].size())) {
+            return false;
+        }
     }
-    
-    return answer;
+    return true;
 }
