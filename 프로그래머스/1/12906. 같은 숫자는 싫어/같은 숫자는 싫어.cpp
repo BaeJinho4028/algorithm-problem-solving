be540a0d6e7df188f649d11fbd3498153f7e005a
배진호 <onionring4028@gmail.com>
2025-02-25 03:30:56 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 108.43 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -4,14 +4,6 @@ using namespace std;
 vector<int> solution(vector<int> arr) {
     ios::sync_with_stdio(0); cin.tie(0);
     
-    vector<int> answer;
-    int pre;
-    for (int n : arr) {
-        if (pre != n) {
-            answer.emplace_back(n);
-        }
-        pre = n;
-    }
-    
-    return answer;
+    arr.erase(unique(arr.begin(), arr.end()), arr.end());
+    return arr;
 }
