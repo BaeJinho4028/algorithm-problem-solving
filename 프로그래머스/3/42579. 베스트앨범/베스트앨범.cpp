9032272ac1ed490c2a6e97cedc2b93bc63fbe303
배진호 <onionring4028@gmail.com>
2025-03-18 20:29:25 +0900

    [level 3] Title: 베스트앨범, Time: 0.07 ms, Memory: 4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
@@ -1,42 +1,48 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 vector<int> solution(vector<string> genres, vector<int> plays) {
     vector<int> answer;
     
-    map<string, int> music;
-    map<string, map<int,int>> mlist;
-    
-    for(int i=0; i < genres.size(); i++){
-        music[genres[i]] += plays[i];
-        mlist[genres[i]][i] = plays[i];
+    // 1. 장르-횟수, 장르-노래(번호)-횟수
+    map<string, int> genres_cnt;
+    map<string, map<int, int>> genres_music_cnt;
+    for (int i = 0; i < genres.size(); ++i) {
+        genres_cnt[genres[i]] += plays[i];
+        genres_music_cnt[genres[i]][i] += plays[i];
     }
     
-    while(music.size() > 0){
+    // 장르 모두 체크
+    while (genres_cnt.size() > 0) {
         string genre = "";
         int mx = 0;
         
-        for(auto m : music){
-            if(mx < m.second){
-                genre = m.first;
-                mx = m.second;
+        // 장르 제일 높은 것 찾기
+        for (auto gc : genres_cnt) {
+            if (mx < gc.second) {
+                genre = gc.first;
+                mx = gc.second;
             }
         }
         
-        for(int i=0; i<2; i++){
-            int tmp = 0, idx = -1;
-            for(auto ml : mlist[genre]){
-                if(tmp < ml.second){
-                    idx = ml.first;
-                    tmp = ml.second;
+        // 2곡 찾기
+        for (int i = 0; i < 2; ++i) {
+            int idx = -1;
+            int cnt = 0;
+            
+            // 노래 중 제일 높은 것 찾기
+            for (auto gmc : genres_music_cnt[genre]) {
+                if (cnt < gmc.second) {
+                    idx = gmc.first;
+                    cnt = gmc.second; 
                 }
             }
-            if(idx == -1) break;
+            
+            if (idx == -1) break;
             answer.push_back(idx);
-            mlist[genre].erase(idx);
+            genres_music_cnt[genre].erase(idx);
         }
-        music.erase(genre);
+        genres_cnt.erase(genre);
     }
     return answer;
 }
