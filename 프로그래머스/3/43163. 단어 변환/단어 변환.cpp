649641353647722099e3d8cf56e3020b643475e6
배진호 <onionring4028@gmail.com>
2025-03-18 22:00:03 +0900

    [level 3] Title: 단어 변환, Time: 0.11 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -1,33 +1,9 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+
 using namespace std;
 
 int solution(string begin, string target, vector<string> words) {
-    unordered_set<string> se(words.begin(), words.end());
-    
-    queue<pair<string, int>> q;
-    unordered_set<string> vis;
-    
-    q.push({begin, 0});
-    vis.insert(begin);
-    
-    while (!q.empty()) {
-        auto cur = q.front(); q.pop();
-        
-        if (cur.first == target) {
-            return cur.second;
-        }
-        
-        for (int i = 0; i < cur.first.size(); ++i) {
-            string nxt = cur.first;
-            for (char c = 'a'; c <= 'z'; c++) {
-                if(nxt[i] == c) continue;
-                nxt[i] = c;
-                if (se.count(nxt) && !vis.count(nxt)) {
-                    q.push({nxt, cur.second + 1});
-                    vis.insert(nxt);
-                }
-            }
-        }
-    }
-    return 0;
+    int answer = 0;
+    return answer;
 }
