507d109cfe8c8129975b6575ea98434644a04fc0
배진호 <onionring4028@gmail.com>
2025-06-01 19:46:54 +0900

    [level 0] Title: A 강조하기, Time: 0.01 ms, Memory: 3.65 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    for (char& c : myString) {
+        if (c == 'a') {
+            c = toupper(c);
+        } else if (c != 'A') {
+            c = tolower(c);
+        }
+    }
+    return myString;
+}
