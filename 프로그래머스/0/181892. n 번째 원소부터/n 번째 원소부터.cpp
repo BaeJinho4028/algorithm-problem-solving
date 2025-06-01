672051c47d1b96dcd1d9e72003c91507a95ad86e
배진호 <onionring4028@gmail.com>
2025-06-01 17:47:29 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.cpp
@@ -0,0 +1,8 @@
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list, int n) {
+    num_list.erase(num_list.begin(), num_list.begin() + n - 1);
+    return num_list;
+}
