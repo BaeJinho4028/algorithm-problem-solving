428e9ec6b6b98359ac424453a45194ae0a6ebe0e
배진호 <onionring4028@gmail.com>
2025-06-14 14:49:28 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.01 ms, Memory: 4.05 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(int a, int b) {
+    if (a % 2 && b % 2) {
+        return a * a + b * b;
+    } else if (a % 2 || b % 2) {
+        return 2 * (a + b);
+    }
+    return abs(a - b);
+}
