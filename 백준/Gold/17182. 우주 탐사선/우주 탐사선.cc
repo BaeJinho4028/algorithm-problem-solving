d4d5216d2dc1e16c1c33f7ace57d6d5213f8b795
배진호 <onionring4028@gmail.com>
2025-07-21 15:30:11 +0900

    [Gold III] Title: 우주 탐사선, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Gold/17182. 우주 탐사선/우주 탐사선.cc
+++ 백준/Gold/17182. 우주 탐사선/우주 탐사선.cc
@@ -1,41 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int d[15][15];
-vector<int> comb;
+int n, m;
+int d[10][10];
+vector<vector<int>> dp(10, vector<int>(1 << 10, -1));
 
-int main() { //
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+int dfs(int cur, int vis) {
+    if (vis == (1 << n) - 1) return 0;
 
-	int n, k;
-	cin >> n >> k;
+    int& ret = dp[cur][vis];
+    if (~ret) return ret;
 
-	for (int i = 0; i < n; i++)
-		if (i != k) comb.push_back(i); //시작점 제외
+    ret = '????';
+    for (int i = 0; i < n; ++i) {
+        if (vis & (1 << i)) continue;
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cin >> d[i][j];
-		}
-	}
+        ret = min(ret, d[cur][i] + dfs(i, vis | (1 << i)));
+    }
+    return ret;
+}
 
-	for (int k = 0; k < n; k++) {
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
-			}
-		}
-	}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-	int ans = 0x7f7f7f7f;
+    cin >> n >> m;
 
-	do {
-		int tmp = d[k][comb[0]];
-		for (int i = 1; i < n - 1; i++)
-			tmp += d[comb[i - 1]][comb[i]];
-		ans = min(tmp, ans);
-	} while (next_permutation(comb.begin(), comb.end()));
+    
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> d[i][j];
+        }
+    }
 
-	cout << ans;
+    for (int k = 0; k < n; ++k) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j< n; ++j) {
+                d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+            }
+        }
+    }
+
+    cout << dfs(m, 1 << m);
 }
