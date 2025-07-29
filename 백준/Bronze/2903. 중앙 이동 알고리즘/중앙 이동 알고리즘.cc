42a313e7975f1cacb1738c02736896e42d8fabe5
배진호 <onionring4028@gmail.com>
2025-07-29 22:42:19 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    
+    int x = (pow(2, n) + 1);
+    cout << x * x;
+}
