f469b7d7d30f52102a42e97f7223d8713297fe33
배진호 <onionring4028@gmail.com>
2025-08-13 10:25:33 +0900

    [Silver II] Title: 좌표 압축, Time: 436 ms, Memory: 9840 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -1,29 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-	
-	int n;
-	cin >> n;
-
-	vector<int> v, u;
-	int x[1000005];
-
-
-	for (int i = 0; i < n; i++) {
-		cin >> x[i];
-		v.push_back(x[i]);
-	}
-	sort(v.begin(), v.end());
-
-	for (int i = 0; i < n; i++) {
-		if (i == 0 || v[i] != v[i - 1])
-			u.push_back(v[i]);
-	}
-	for (int i = 0; i < n; i++) {
-		cout << lower_bound(u.begin(), u.end(), x[i]) - u.begin() << ' ';
-	}
+    int n;
+    cin >> n;
+    vector<int> v(n), uni(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+        uni[i] = v[i];
+    }
+    sort(uni.begin(), uni.end());
+    uni.erase(unique(uni.begin(), uni.end()), uni.end());
+    
+    for (int i = 0; i < n; ++i) {
+        cout << lower_bound(uni.begin(), uni.end(), v[i]) - uni.begin() << ' ';
+    }
 }
