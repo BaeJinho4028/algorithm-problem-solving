4b2c2072a98e9c58b6584c3b99c1b290a2eaee33
배진호 <onionring4028@gmail.com>
2025-09-08 23:20:03 +0900

    [Gold IV] Title: List of Unique Numbers, Time: 8 ms, Memory: 2544 KB -BaekjoonHub

--- 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.cc
+++ 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.cc
@@ -1,30 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int a[100005];
-int mn = 0x7fffffff;
-
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	
-	int n;
-	cin >> n;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-	vector<bool> chk(100005);
-	for (int i = 0; i < n; i++) cin >> a[i];
+    int n;
+    cin >> n;
 
-	long long ans = 0;
-	chk[a[0]] = 1;
-	int en = 0;
-	for (int st = 0; st < n; st++) {
-		while (en < n-1 && !chk[a[en+1]]) {
-			en++;
-			chk[a[en]] = 1;
-		}
-		ans += (en - st + 1);
-		chk[a[st]] = 0;
-	}
+    vector<int> v(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+    }
 
-	cout << ans;
+    vector<bool> chk(100'001, false);
+
+    long long ans = 0;
+
+    int st = 0, en = 0;
+    while (st < n) {
+        while (en < n && !chk[v[en]]) {
+            chk[v[en++]] = true;
+        }
+        ans += (en - st);
+        chk[v[st++]] = false;
+    }
+
+    cout << ans;
 }
