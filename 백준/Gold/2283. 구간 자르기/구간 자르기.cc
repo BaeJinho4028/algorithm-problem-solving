be5e53079ad4858c01332d3c8e8a52d286802716
배진호 <onionring4028@gmail.com>
2025-09-11 11:19:29 +0900

    [Gold II] Title: 구간 자르기, Time: 408 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Gold/2283. 구간 자르기/구간 자르기.cc
+++ 백준/Gold/2283. 구간 자르기/구간 자르기.cc
@@ -1,34 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int a[1000005];
-
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-	int n, k;
-	cin >> n >> k;
+    int n, k;
+    cin >> n >> k;
+    
+    vector<int> v(1'000'001);
+    for (int i = 0; i < n; ++i) {
+        int a, b;
+        cin >> a >> b;
 
-	for (int i = 0; i < n; i++) {
-		int l, r;
-		cin >> l >> r;
+        for (int j = a; j < b; ++j) {
+            v[j]++;
+        }
+    }
 
-		for (int j = l; j < r; j++) {
-			a[j]++;
-		}
-	}
-
-	int st = 0, en = 0;
-	int sum = 0;
-	while (st <= en && en <= 1000000) {
-		if (sum == k) {
-			cout << st << ' ' << en;
-			return 0;
-		}
-		else if (sum < k) sum += a[en++];
-		else sum -= a[st++];
-		
-	}
-	cout << 0 << ' ' << 0;
+    int st = 0, en = 0;
+    int sum = 0;
+    const int MAX = 1'000'000;
+    while (st < MAX) {
+        while (sum < k && en <= MAX) {
+            sum += v[en++];
+        }
+        if (sum == k) {
+            cout << st << ' ' << en;
+            return 0;
+        }
+        sum -= v[st++];
+    }
+    cout << "0 0";
 }
