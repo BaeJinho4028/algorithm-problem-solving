afb7dc250c076a0f5abae2825a225ab3defb75fb
배진호 <onionring4028@gmail.com>
2025-09-16 11:15:33 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 8 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long d[1000005];
+int n;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	d[1] = 1;
+	d[2] = 2;
+	d[3] = 4;
+
+	for (int i = 4; i <= 1000000; i++) {
+		d[i] = (d[i - 1] + d[i - 2] + d[i - 3]) % 1000000009;
+	}
+
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		cout << d[n] << '\n';
+	}
+}
