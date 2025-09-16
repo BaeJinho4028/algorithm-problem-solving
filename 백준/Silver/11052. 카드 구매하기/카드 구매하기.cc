b22cd2584fb9a771acda5af7a986966ea39893fa
배진호 <onionring4028@gmail.com>
2025-09-16 18:18:13 +0900

    [Silver I] Title: 카드 구매하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    vector<int> d(n + 1);
+    for (int i = 1; i <= n; ++i) {
+        cin >> d[i];
+    }
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j < i; ++j) {
+            d[i] = max(d[i], d[i - j] + d[j]);
+        }
+    }
+    cout << d[n];
+}
