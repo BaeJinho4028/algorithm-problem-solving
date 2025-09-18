29822073855e317920014d58b018eb70c09ce3af
배진호 <onionring4028@gmail.com>
2025-09-18 10:46:02 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -1,39 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m;
-int arr[10];
-bool is[10];
-
-void func(int cur) {
-	if (cur == m) {
-		for (int i = 0; i < m; i++) {
-			cout << arr[i] << ' ';
-		}
-		cout << '\n';
-
-		return;
-	}
-
-	int st = 1;
-	if (cur != 0) st = arr[cur - 1] + 1;
-
-	for (int i = st; i <= n; i++) {
-		if (!is[i]) {
-			arr[cur] = i;
-			is[i] = 1;
-			func(cur + 1);
-			is[i] = 0;
-		}
-	}
-}
-
-
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-	cin >> n >> m;
+    int n, m;
+    cin >> n >> m;
 
-	func(0);
+    vector<int> v(n, 1);
+    fill(v.begin(), v.begin() + m, 0);
+
+    do {
+        for (int i = 0; i < n; ++i) {
+            if (v[i] == 0) cout << i + 1 << ' ';
+        }
+        cout << '\n';
+    } while (next_permutation(v.begin(), v.end()));
 }
