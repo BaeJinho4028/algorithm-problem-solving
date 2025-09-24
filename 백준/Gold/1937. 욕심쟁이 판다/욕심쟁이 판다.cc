e8ea557655461e572a66dce5b0ff270705713384
배진호 <onionring4028@gmail.com>
2025-09-24 22:22:27 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 32 ms, Memory: 4192 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.cc
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.cc
@@ -1,44 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, mx;
-int board[505][505];
-int d[505][505];
-int dx[4] = { 1,0,-1,0 };
-int dy[4] = { 0,1,0,-1 };
+int n;
+int board[502][502];
+int d[502][502];
 
 int dfs(int x, int y) {
-	if (d[x][y] != 0) return d[x][y];
-	d[x][y] = 1;
+    if (d[x][y] != 0) return d[x][y];
+    d[x][y] = 1;
 
-	for (int dir = 0; dir < 4; dir++) {
-		int nx = x + dx[dir];
-		int ny = y + dy[dir];
+    int dx[4] = {1, 0, -1, 0};
+    int dy[4] = {0, 1, 0, -1};
 
-		if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-		if (board[nx][ny] > board[x][y])
-			d[x][y] = max(d[x][y], dfs(nx, ny) + 1);
-	}
+    for (int dir = 0; dir < 4; ++dir) {
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
 
-	return d[x][y];
+        if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+        if (board[nx][ny] > board[x][y]) {
+            d[x][y] = max(d[x][y], dfs(nx, ny) + 1);
+        }
+    }
+    return d[x][y];
 }
 
-int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cin >> board[i][j];
-		}
-	}
+    cin >> n;
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			mx = max(mx, dfs(i, j));
-		}
-	}
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> board[i][j];
+        }
+    }
 
-	cout << mx;
+    int mx = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            mx = max(mx, dfs(i, j));
+        }
+    }
+
+    cout << mx;
 }
