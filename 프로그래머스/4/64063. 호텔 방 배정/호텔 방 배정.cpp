f01f78252da72980e9a9a1f06463aafb9ad9df21
배진호 <onionring4028@gmail.com>
2025-10-06 04:11:35 +0900

    [level 4] Title: 호텔 방 배정, Time: 158.75 ms, Memory: 60.6 MB -BaekjoonHub

--- 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.cpp
+++ 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.cpp
@@ -2,23 +2,20 @@
 
 using namespace std;
 
-unordered_map<long long, long long> next_room;
+unordered_map<long long, long long> um;
 
-long long f(long long r) {
-    if (next_room.find(r) == next_room.end()) {
-        next_room[r] = r + 1;
-        return r;
-    }
-    return next_room[r] = f(next_room[r]);
+long long find(long long x) {
+    if (um.find(x) == um.end()) return x;
+    else return um[x] = find(um[x]);
 }
 
 vector<long long> solution(long long k, vector<long long> room_number) {
     vector<long long> answer;
     answer.reserve(room_number.size());
-    
-    for (auto r : room_number) {
-        long long x = f(r);
-        answer.push_back(x);
+    for (long long x : room_number) {
+        long long y = find(x);
+        answer.push_back(y);
+        um[y] = y + 1;
     }
     return answer;
 }
