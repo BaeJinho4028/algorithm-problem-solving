c581e8815fc432a91e0fd7434f2e707411f62672
배진호 <onionring4028@gmail.com>
2026-02-03 11:25:32 +0900

    [Silver III] Title: 작은 정사각형 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14223. 작은 정사각형 1/작은 정사각형 1.cc
+++ 백준/Silver/14223. 작은 정사각형 1/작은 정사각형 1.cc
@@ -8,33 +8,32 @@ int main() {
     int n;
     cin >> n;
 
-    vector<long long> x(n), y(n);
-    for (int i = 0; i < n; i++) {
+    vector<long long> x(n), y(n); 
+    for (int i = 0; i < n; ++i) {
         cin >> x[i] >> y[i];
     }
 
     long long ans = LLONG_MAX;
 
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 1; j < n; ++j) {
 
-            long long minX = LLONG_MAX, maxX = LLONG_MIN;
-            long long minY = LLONG_MAX, maxY = LLONG_MIN;
+            long long mnX = LLONG_MAX, mxX = LLONG_MIN;
+            long long mnY = LLONG_MAX, mxY = LLONG_MIN;
 
-            for (int k = 0; k < n; k++) {
+            for (int k = 0; k < n; ++k) {
                 if (k == i || k == j) continue;
 
-                minX = min(minX, x[k]);
-                maxX = max(maxX, x[k]);
-                minY = min(minY, y[k]);
-                maxY = max(maxY, y[k]);
+                mnX = min(mnX, x[k]);
+                mxX = max(mxX, x[k]);
+                mnY = min(mnY, y[k]);
+                mxY = max(mxY, y[k]);
             }
-
-            long long side = max(maxX - minX, maxY - minY) + 2;
-            ans = min(ans, side * side);
+            
+            long long e = max(mxX - mnX, mxY - mnY) + 2;
+            ans = min(ans, e * e);
         }
     }
 
     cout << ans;
-    return 0;
 }
