b4d6b27aab656b473a234598e5c9c47232166b6e
배진호 <onionring4028@gmail.com>
2026-03-26 11:37:34 +0900

    [Gold III] Title: 경사로, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.cc
+++ 백준/Gold/14890. 경사로/경사로.cc
@@ -1,57 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, l;
-int board[105][105];
-int ans;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-/*
-정리습관
+    int n, l;
+    cin >> n >> l;
 
-1. 경사로 겹치면 안됨
-2. 올라가는경우 연속된층 L만큼있었는지
-3. 내려가는경우 연속된층이 L만큼 있을지
-*/
+    vector<vector<int>> v(n, vector<int>(n));
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cin >> v[i][j];
+        }
+    }
 
-int main(void) {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+    int ans = 0;
+    for (int i = 0; i < n; ++i) {
+        int cnt = 1;
+        bool flag = true;
 
-	cin >> n >> l;
+        for (int j = 1; j < n; ++j) {
+            if (v[i][j - 1] == v[i][j]) cnt++;
+            else if (v[i][j - 1] - v[i][j] == -1 && cnt >= l) cnt = 1;
+            else if (v[i][j - 1] - v[i][j] == 1 && cnt >= 0) cnt = -l + 1;
+            else flag = false;
+        }
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cin >> board[i][j];
-		}
-	}
+        if (flag && cnt >= 0) {
+            ans++;
+        }
+    }
 
-	for (int i = 0; i < n; i++) {
-		bool flag = true;
-		int cnt = 1;
-		
-		for (int j = 1; j < n; j++) {
-			if (board[i][j - 1] == board[i][j]) cnt++;
-			else if (board[i][j - 1] - board[i][j] == -1 && cnt >= l) cnt = 1;
-			else if (board[i][j - 1] - board[i][j] == 1 && cnt >= 0) cnt = -l + 1;
-			else flag = false;
-		}
+    for (int i = 0; i < n; ++i) {
+        int cnt = 1;
+        bool flag = true;
 
-		if (flag && cnt >= 0) ans++;
-	}
+        for (int j = 1; j < n; ++j) {
+            if (v[j - 1][i] == v[j][i]) cnt++;
+            else if (v[j - 1][i] - v[j][i] == -1 && cnt >= l) cnt = 1;
+            else if (v[j - 1][i] - v[j][i] == 1 && cnt >= 0) cnt = -l + 1;
+            else flag = false;
+        }
 
-	for (int i = 0; i < n; i++) {
-		bool flag = true;
-		int cnt = 1;
+        if (flag && cnt >= 0) {
+            ans++;
+        }
+    }
 
-		for (int j = 1; j < n; j++) {
-			if (board[j-1][i] == board[j][i]) cnt++;
-			else if (board[j-1][i] - board[j][i] == -1 && cnt >= l) cnt = 1;
-			else if (board[j-1][i] - board[j][i] == 1 && cnt >= 0) cnt = -l + 1;
-			else flag = false;
-		}
-
-		if (flag && cnt >= 0) ans++;
-	}
-
-	cout << ans;
+    cout << ans;
 }
+
