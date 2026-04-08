c3ed31c9a6020e17c534e3afa20f9fb113f65e0e
배진호 <onionring4028@gmail.com>
2026-04-08 17:29:20 +0900

    [Gold III] Title: 파티, Time: 188 ms, Memory: 2176 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -5,6 +5,7 @@ vector<pair<int,int>> adj[1002];
 
 int solve(int st, int en) {
     vector<int> d(1002, '????');
+
     priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
     d[st] = 0;
     pq.push({d[st], st});
@@ -13,6 +14,7 @@ int solve(int st, int en) {
         auto [cw, cu] = pq.top(); pq.pop();
 
         if (d[cu] != cw) continue;
+
         for (auto [nw, nv] : adj[cu]) {
             if (d[nv] > d[cu] + nw) {
                 d[nv] = d[cu] + nw;
@@ -27,8 +29,8 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int n, m, en;
-    cin >> n >> m >> en;
+    int n, m, x;
+    cin >> n >> m >> x;
 
     while (m--) {
         int u, v, w;
@@ -37,8 +39,8 @@ int main() {
     }
 
     int ans = 0;
-    for (int st = 1; st <= n; ++st) {
-        ans = max(ans, solve(st, en) + solve(en, st));
+    for (int i = 1; i <= n; ++i) {
+        ans = max(ans, solve(i, x) + solve(x, i));
     }
     cout << ans;
 }
